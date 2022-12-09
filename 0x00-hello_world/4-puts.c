eacca3d120c650afd86c1a88c3d8bc7387bb9d0a
GREAT-D <ifeanyindukwu987@gmail.com>
2022-12-09 02:01:34 -0800

    first c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
- * main - print a code line using puts
+ * main - entry point a line of code using puts
  *
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
