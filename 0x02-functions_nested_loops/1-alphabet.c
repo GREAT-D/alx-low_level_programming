0c79ed5a86b054b8d200aa78b8b3c0b4f5692a71
GREAT-D <ifeanyindukwu987@gmail.com>
2022-12-14 17:21:28 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - makwe the alphabet
+ * Returns: void
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c= 'e'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	_putchar('\n');
+}
